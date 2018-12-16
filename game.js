1bd72f2d1c0afb92a86b3fc88ca4809c9665360f
melius18 <melius18@protonmail.com>
2018-12-16 23:50:42 +0900

    apply policy

--- game.js
+++ game.js
@@ -1,7 +1,16 @@
-let turn = 0;       // 1: Navy, -1: Grey
+let turn = 0;       // Grey: 1, Navy: -1
 let turn_num = 0;
 let bd_arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 let td_arr = [td0, td1, td2, td3, td4, td5, td6, td7, td8];
+let com_turn = 0;
+let full_auto = 1;  // com vs com
+let auto_start = 1; // auto repeat
+let state_seq = [];
+let greywin = 0;
+let navywin = 0;
+let drawgam = 0;
+
+let dt = 50;
 
 // show state
 btn3.onclick = function () {
@@ -14,10 +23,12 @@ btn3.onclick = function () {
 btn2.onclick = function () {
     turn = 0;
     turn_num = 0;
+    state_seq = [];
     for (let i = 0; i < td_arr.length; i++) bd_arr[i] = 0;
     btn2.hidden = "none";
     btn1.hidden = "";
     exp_tb();
+    if (auto_start == 1) setTimeout(function () { btn1.onclick(); }, dt * 3);
 }
 
 // gaming(turn != 0)
@@ -26,6 +37,8 @@ btn1.onclick = function () {
     turn = 1;
     btn1.hidden = "none";
     btn2.hidden = "";
+    com_turn = 1;
+    if (com_turn == turn) com_action();
 }
 
 // assign event handler.
@@ -36,18 +49,19 @@ function click_td() {
     num = parseInt(this.id.substr(-1, 1));
     if (bd_arr[num] == 0 && turn != 0) {
         bd_arr[num] = turn;
+        record_seq();
         turn = -turn;
         turn_num++;
         exp_tb();   // express table color
-        bd_state(); // chech board state 
+        bd_state(); // check board state 
     }
 }
 
 // express table color
 function exp_tb() {
     for (let i = 0; i < bd_arr.length; i++) {
-        if (bd_arr[i] == 1) td_arr[i].style.backgroundColor = "navy";
-        else if (bd_arr[i] == -1) td_arr[i].style.backgroundColor = "grey";
+        if (bd_arr[i] == -1) td_arr[i].style.backgroundColor = "navy";
+        else if (bd_arr[i] == 1) td_arr[i].style.backgroundColor = "grey";
         else td_arr[i].style.backgroundColor = "white";
     }
 }
@@ -63,10 +77,21 @@ function bd_state() {
     if (tmp > 0.5) setTimeout(function () { btn2.onclick(); }, 0);
     else if (turn_num == 9) {
         // draw
-        setTimeout(function () { alert("Draw!"); }, 0);
+        reward(-100);
+        drawgam++;
+        setTimeout(function () { inform("Draw!"); }, 0);
         setTimeout(function () { btn2.onclick(); }, 0);
     }
     else {
+        // not end
+        if (com_turn == turn) com_action();
+        else if (full_auto == 1) {
+            setTimeout(function () {
+                com_action();
+                // console.log("2");
+            }, dt);
+            // console.log("1");
+        }
     }
 }
 
@@ -74,14 +99,45 @@ function bd_state() {
 function chkLine(num) {
     if (num == 3 || num == -3) {
         if (num == 3) {
-            // navy win
-            setTimeout(function () { alert("Navy Win!"); }, 0);
+            // grey win
+            reward(1000);
+            greywin++;
+            setTimeout(function () { inform("Grey Win!"); }, 0);
         }
         else {
-            // grey win
-            setTimeout(function () { alert("Grey Win!"); }, 0);
+            // navy win
+            reward(-1000);
+            navywin++;
+            setTimeout(function () { inform("Navy Win!"); }, 0);
         }
         return 1;
     }
     else return 0;
+}
+
+function com_action() {
+    let num;
+    if (turn == -1) num = navy_policy();
+    else if (turn == 1) num = grey_policy();
+
+    if (bd_arr[num] == 0 && turn != 0) {
+        bd_arr[num] = turn;
+        record_seq();
+        turn = -turn;
+        turn_num++;
+        exp_tb();   // express table color
+        bd_state(); // chech board state 
+    }
+}
+
+
+
+function inform(msg) {
+    // console.log(bd_arr.slice(0, 3));
+    // console.log(bd_arr.slice(3, 6));
+    // console.log(bd_arr.slice(6, 9));
+    console.log(msg);
+    console.log(state_id.length);
+    console.log("grey: ", greywin, "navy: ", navywin, "draw game: ", drawgam)
+    // alert(msg);
 }
---
+++ rlearing.js
@@ -0,0 +1,109 @@
+let state_id = [], state_val = [];
+let alpha = 0.1;
+let beta_g = 0.75;     // 0: rnd   1: greedy
+let beta_n = 0.75;     // 0: rnd   1: greedy
+
+function get_state_num(arr) {
+    let bd_num = 0;
+    for (let i = 0; i < arr.length; i++) {
+        bd_num = bd_num + (arr[i] < 0 ? 2 : arr[i]) * Math.pow(3, i);
+    }
+    return bd_num;
+}
+
+function reward(point) {
+    let state_seq_tmp = JSON.parse(JSON.stringify(state_seq));
+    let idx = state_id.indexOf(state_seq_tmp.pop());
+    state_val[idx] = state_val[idx] + alpha * (point - state_val[idx]);
+    let idx2 = idx;
+    let num = state_seq_tmp.length;
+    for (let i = 0; i < num; i++) {
+        idx = state_id.indexOf(state_seq_tmp.pop());
+        state_val[idx] = state_val[idx] + alpha * (state_val[idx2] - state_val[idx]);
+        idx2 = idx;
+    }
+    // console.log(state_id);
+    // console.log(state_val);
+}
+
+function record_seq() {
+    let bd_state_num = get_state_num(bd_arr);
+    let idx = state_id.indexOf(bd_state_num);
+    if (idx == -1) {
+        state_id.push(bd_state_num);
+        state_val.push(0);
+    }
+    state_seq.push(bd_state_num);
+}
+
+function grey_policy() {
+
+    let rnd_num;
+    do {
+        rnd_num = Math.floor(Math.random() * 9.0);
+        // console.log("3");
+    } while (bd_arr[rnd_num] != 0)
+
+    let max_num = -1;
+    let max_val = state_val.length == 0 ? 0 : state_val.reduce(function (previous, current) {
+        return previous > current ? (current - 1) : previous;
+    });
+
+    for (let i = 0; i < bd_arr.length; i++) {
+        let bd_arr_tmp = JSON.parse(JSON.stringify(bd_arr));
+        if (bd_arr[i] == 0) {
+            bd_arr_tmp[i] = 1;
+            let idx = state_id.indexOf(get_state_num(bd_arr_tmp));
+            if (idx != -1 && state_val[idx] > max_val) {
+                max_val = state_val[idx];
+                max_num = i;
+            }
+        }
+    }
+
+    if (max_num == -1) max_num = rnd_num;
+
+    let num;
+    if (Math.random() > beta_g) num = rnd_num;
+    else {
+        // console.log("greedy_g");
+        num = max_num;
+    }
+    return num;
+}
+
+function navy_policy() {
+
+    let rnd_num;
+    do {
+        rnd_num = Math.floor(Math.random() * 9.0);
+        // console.log("3");
+    } while (bd_arr[rnd_num] != 0)
+
+    let min_num = -1;
+    let min_val = state_val.length == 0 ? 0 : state_val.reduce(function (previous, current) {
+        return previous > current ? previous : (current + 1);
+    });
+
+    for (let i = 0; i < bd_arr.length; i++) {
+        let bd_arr_tmp = JSON.parse(JSON.stringify(bd_arr));
+        if (bd_arr[i] == 0) {
+            bd_arr_tmp[i] = -1;
+            let idx = state_id.indexOf(get_state_num(bd_arr_tmp));
+            if (idx != -1 && state_val[idx] < min_val) {
+                min_val = state_val[idx];
+                min_num = i;
+            }
+        }
+    }
+
+    if (min_num == -1) min_num = rnd_num;
+
+    let num;
+    if (Math.random() > beta_n) num = rnd_num;
+    else {
+        // console.log("greedy_n");
+        num = min_num;
+    }
+    return num;
+}
